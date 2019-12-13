c9074244f848920552f8ba9268d309e37c9ee2ad
matthewkayin <matthewkayin@gmail.com>
2019-12-12 21:35:55 -0600

    Working site build script with some example posts
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

--- build.py
+++ build.py
@@ -2,6 +2,8 @@
 # The idea is to give me the convenience of creating new blog posts without having
 # to edit the code and without having to commit to any sort of backend
 
+import os
+
 
 def open_post(path):
     post_file = open(path, "r")
@@ -41,31 +43,90 @@ def create_post_div(post):
     div = []
 
     content = get_post_content(post)
-    div.append("""\t\t\t<div class="post white">""")
-    div.append("""\t\t\t\t<p class="text headline">""" + get_post_title(post) + "</p>")
-    div.append("""\t\t\t\t<p class="text date">""" + get_post_date(post) + "</p>")
-    div.append("""\t\t\t\t<p class="text content">""" + content[0])
+    div.append("\t\t\t<div class=\"post white\">")
+    div.append("\t\t\t\t<p class=\"text headline\">" + get_post_title(post) + "</p>")
+    div.append("\t\t\t\t<p class=\"text date\">" + get_post_date(post) + "</p>")
+    div.append("\t\t\t\t<p class=\"text content\">" + content[0])
     for i in range(1, len(content) - 1):
         div.append("\t\t\t\t\t\t\t" + content[i])
     div.append("\t\t\t\t\t\t\t" + content[len(content) - 1] + "</p>")
-    div.append("""\t\t\t</div>""")
+    div.append("\t\t\t</div>")
 
     return div
 
 
-def create_post_file(shortname, div):
-    template_file = open("article.html", "r")
+def create_month_div(month_list):
+    # let's format the month lists as ["Month YYYY", [Title, shortname], ...]
+    # this way we only store the month once but we still get a list
+    div = []
+
+    div.append("\t\t\t<div class=\"post white\">")
+    div.append("\t\t\t\t<p class=\"text headline\">" + month_list[0] + "</p>")
+    for i in range(1, len(month_list)):
+        div.append("\t\t\t\t<p class=\"text content\"><a href=\"" + month_list[i][1] + ".html\">" + month_list[i][0] + "</a></p>")
+    div.append("\t\t\t</div>")
+
+    return div
+
+
+def create_from_template(template_path, dest_path, divs):
+    template_file = open(template_path)
     template = template_file.read().splitlines()
 
-    output = template[0:template.index("REPLACE")] + div + template[template.index("REPLACE") + 1::]
-    post_file = open(shortname + ".html", "w")
+    output = template[0:template.index("REPLACE")]
+    for div in divs:
+        output = output + div
+    output = output + template[template.index("REPLACE") + 1::]
+
+    post_file = open(dest_path, "w")
     post_file.writelines("\n".join(output))
 
 
-# test some stuff
-post = open_post("posts/1.txt")
-shortname = get_post_shortname(post)
-month = get_post_month(post)
-div = create_post_div(post)
-create_post_file(shortname, div)
-print(month)
+print("Hello! Welcome to the extremely specific blog compiler!")
+print("Compiling website...")
+recent_posts = []
+month_lists = []
+
+num_posts = len([name for name in os.listdir("posts") if os.path.isfile("posts/" + str(name))])
+for i in range(0, num_posts):
+    post_num = i + 1
+    post_file = "posts/" + str(post_num) + ".txt"
+    print("Reading " + post_file + "...", end=" ")
+    post = open_post(post_file)
+
+    shortname = get_post_shortname(post)
+    title = get_post_title(post)
+    month = get_post_month(post)
+
+    # If current post's month in list add the post to the list
+    # else create a new month list for this month
+    if month in [month_list[0] for month_list in month_lists]:
+        location = [month_list[0] for month_list in month_lists].index(month)
+        month_lists[location].insert(1, [title, shortname])
+    else:
+        month_lists.append([month, [title, shortname]])
+
+    div = create_post_div(post)
+
+    if len(recent_posts) < 10:
+        recent_posts.insert(0, div)
+    else:
+        recent_posts.pop()
+        recent_posts.insert(0, div)
+
+    create_from_template("article.html", shortname + ".html", [div])
+    print("Done!")
+
+print("Generating index...", end=" ")
+create_from_template("index-template.html", "index.html", recent_posts)
+print("Done!")
+
+print("Generating archive...", end=" ")
+month_divs = []
+for month_list in month_lists:
+    month_divs.insert(0, create_month_div(month_list))
+
+create_from_template("archive-template.html", "archive.html", month_divs)
+print("Done!")
+
+print("Website build complete! Maybe use wordpress next time?")
